6c938575b526fdb2fcad6ce8d257ef747459dfb9
shinde-s <suhas.shinde@xoriant.com>
2014-05-19 16:58:57 +0530

    first changes commiting

--- Practicale/src/test/RemoveIndex.java
+++ Practicale/src/test/RemoveIndex.java
@@ -4,7 +4,7 @@ public class RemoveIndex {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("welcome to git");
 		String str = "acb(12345)";
 		int cnt = str.indexOf("(");
 		String str1 = str.substring(cnt+1, (str.length()-1));
